6d5b9108fcc88cdd6ab930bfb11fd56d366421a1
SabillonJ <161340097+SabillonJ@users.noreply.github.com>
2024-02-27 15:17:16 -0600

    lo mejor

--- Sistema_Cine/Sistema_Cine/Controllers/tbGenerosController.cs
+++ Sistema_Cine/Sistema_Cine/Controllers/tbGenerosController.cs
@@ -10,112 +10,112 @@ using Sistema_Cine.Models;
 
 namespace Sistema_Cine.Controllers
 {
-    public class tbMunicipiosController : Controller
+    public class tbGenerosController : Controller
     {
         private dbSsitemascinesEntities4 db = new dbSsitemascinesEntities4();
 
-        // GET: tbMunicipios
+        // GET: tbGeneros
         public ActionResult Index()
         {
-            var tbMunicipio = db.tbMunicipio.Include(t => t.tbDepartamentos);
-            return View(tbMunicipio.ToList());
+            var tbGeneros = db.tbGeneros.Include(t => t.tbPromociones);
+            return View(tbGeneros.ToList());
         }
 
-        // GET: tbMunicipios/Details/5
-        public ActionResult Details(string id)
+        // GET: tbGeneros/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbMunicipio tbMunicipio = db.tbMunicipio.Find(id);
-            if (tbMunicipio == null)
+            tbGeneros tbGeneros = db.tbGeneros.Find(id);
+            if (tbGeneros == null)
             {
                 return HttpNotFound();
             }
-            return View(tbMunicipio);
+            return View(tbGeneros);
         }
 
-        // GET: tbMunicipios/Create
+        // GET: tbGeneros/Create
         public ActionResult Create()
         {
-            ViewBag.Depa_Codigo = new SelectList(db.tbDepartamentos, "Depa_Codigo", "Depa_Descripcion");
+            ViewBag.Prom_Id = new SelectList(db.tbPromociones, "Prom_Id", "Prom_Descripcion");
             return View();
         }
 
-        // POST: tbMunicipios/Create
+        // POST: tbGeneros/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
         // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Muni_Codigo,Muni_Descripcion,Depa_Codigo,Muni_Usuario_Creacion,Muni_Fecha_Creacion,Muni_Usuario_Modificacion,Muni_Fecha_Modificacion")] tbMunicipio tbMunicipio)
+        public ActionResult Create([Bind(Include = "Gene_Id,Gene_Descripcion,Prom_Id,Gene_Usuario_Creacion,Gene_Fecha_Creacion,Gene_Usuario_Modificacion,Gene_Fecha_Modificacion")] tbGeneros tbGeneros)
         {
             if (ModelState.IsValid)
             {
-                db.tbMunicipio.Add(tbMunicipio);
+                db.tbGeneros.Add(tbGeneros);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.Depa_Codigo = new SelectList(db.tbDepartamentos, "Depa_Codigo", "Depa_Descripcion", tbMunicipio.Depa_Codigo);
-            return View(tbMunicipio);
+            ViewBag.Prom_Id = new SelectList(db.tbPromociones, "Prom_Id", "Prom_Descripcion", tbGeneros.Prom_Id);
+            return View(tbGeneros);
         }
 
-        // GET: tbMunicipios/Edit/5
-        public ActionResult Edit(string id)
+        // GET: tbGeneros/Edit/5
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbMunicipio tbMunicipio = db.tbMunicipio.Find(id);
-            if (tbMunicipio == null)
+            tbGeneros tbGeneros = db.tbGeneros.Find(id);
+            if (tbGeneros == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.Depa_Codigo = new SelectList(db.tbDepartamentos, "Depa_Codigo", "Depa_Descripcion", tbMunicipio.Depa_Codigo);
-            return View(tbMunicipio);
+            ViewBag.Prom_Id = new SelectList(db.tbPromociones, "Prom_Id", "Prom_Descripcion", tbGeneros.Prom_Id);
+            return View(tbGeneros);
         }
 
-        // POST: tbMunicipios/Edit/5
+        // POST: tbGeneros/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
         // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Muni_Codigo,Muni_Descripcion,Depa_Codigo,Muni_Usuario_Creacion,Muni_Fecha_Creacion,Muni_Usuario_Modificacion,Muni_Fecha_Modificacion")] tbMunicipio tbMunicipio)
+        public ActionResult Edit([Bind(Include = "Gene_Id,Gene_Descripcion,Prom_Id,Gene_Usuario_Creacion,Gene_Fecha_Creacion,Gene_Usuario_Modificacion,Gene_Fecha_Modificacion")] tbGeneros tbGeneros)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(tbMunicipio).State = EntityState.Modified;
+                db.Entry(tbGeneros).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.Depa_Codigo = new SelectList(db.tbDepartamentos, "Depa_Codigo", "Depa_Descripcion", tbMunicipio.Depa_Codigo);
-            return View(tbMunicipio);
+            ViewBag.Prom_Id = new SelectList(db.tbPromociones, "Prom_Id", "Prom_Descripcion", tbGeneros.Prom_Id);
+            return View(tbGeneros);
         }
 
-        // GET: tbMunicipios/Delete/5
-        public ActionResult Delete(string id)
+        // GET: tbGeneros/Delete/5
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbMunicipio tbMunicipio = db.tbMunicipio.Find(id);
-            if (tbMunicipio == null)
+            tbGeneros tbGeneros = db.tbGeneros.Find(id);
+            if (tbGeneros == null)
             {
                 return HttpNotFound();
             }
-            return View(tbMunicipio);
+            return View(tbGeneros);
         }
 
-        // POST: tbMunicipios/Delete/5
+        // POST: tbGeneros/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            tbMunicipio tbMunicipio = db.tbMunicipio.Find(id);
-            db.tbMunicipio.Remove(tbMunicipio);
+            tbGeneros tbGeneros = db.tbGeneros.Find(id);
+            db.tbGeneros.Remove(tbGeneros);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
